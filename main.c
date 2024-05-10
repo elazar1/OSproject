4fd2399db3bf9aceafd08a2693abedd3e36aaf2c
erinlazar1 <erin.lazar@student.upt.ro>
2024-05-10 14:39:25 +0300

    mod la captureSnapshot

--- main.c
+++ main.c
@@ -14,11 +14,21 @@ typedef struct Node{
 
 // Function to capture initial snapshot
 void captureSnapshot(const char* directory){
+    // Open the directory
+    DIR* dir = opendir(directory);
+    if(dir == NULL){
+        perror("Unable to open the directory.");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create/open Snapshot.txt for writing in outputDir
+    char snapshotFilePath[512];
+    
 
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
     if(argc != 2){
         printf("Usage: %s <directory>\n", argv[0]);
         return 1;
