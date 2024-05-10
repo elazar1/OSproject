e2f9b83e46b3a28ede44e1da8c8e56559f455516
erinlazar1 <erin.lazar@student.upt.ro>
2024-05-10 14:25:42 +0300

    added captureSnapshot funct

--- main.c
+++ main.c
@@ -3,7 +3,25 @@
 #include <string.h>
 #include <dirent.h>
 
-int main(){
+typedef struct {
+    char name[256];
+} Metadata;
 
+typedef struct Node{
+    Metadata data;
+    struct Node* next;    
+} Node;
+
+// Function to capture initial snapshot
+void captureSnapshot(const char* directory){
+
+}
+
+
+int main(){
+    if(argc != 2){
+        printf("Usage: %s <directory>\n", argv[0]);
+        return 1;
+    }
     return 0;
 }
